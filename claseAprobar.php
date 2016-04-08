ed3f1cf4fc33851a7613253963f1463a35fb02ba
walter leonardo <walterleonardo@gmail.com>
2016-04-08 23:28:05 +0200

    ################## Last version fixed: Date: Autor: Walter Leonardo eMail: walterleonardo@gmail.com Info:
    Bug Info:
    
    
    Solution:
    
    ###################

---
+++ claseAprobar.php
@@ -0,0 +1,37 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class RemoteConnect
+{
+  public function connectToServer($serverName=null)
+  {
+    if($serverName==null){
+      throw new Exception("¡Este no es un nombre de servidor!");
+    }
+    $fp = fsockopen($serverName,80);
+    return ($fp) ? true : false;
+  }
+  
+    public function connectToServer2($serverName=null)
+  {
+    if($serverName==null){
+      throw new Exception("¡Este no es un nombre de servidor!");
+    }
+    $fp = fsockopen($serverName,80);
+    return ($fp) ? true : false;
+  }
+  
+  public function returnSampleObject()
+  {
+    return $this;
+  }
+  
+  public function incrementSalary( $actualSalary ){  
+    return ( 2 * $actualSalary ) /  100;  
+}  
+}
---
+++ testing/claseAprobarTest.php
@@ -0,0 +1,49 @@
+<?php
+
+include '/Applications/MAMP/htdocs/PHPunitTesting/claseAprobar.php';
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of claseAprobarTest
+ *
+ * @author walterleonardo
+ */
+class claseAprobarTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @var \RemoteWebDriver
+     */
+    public function setUp() {
+        
+    }
+
+    public function tearDown() {
+        
+    }
+
+    public function testConnectionIsValid() {
+        // prueba para asegurarse de que el objeto de un fsockopen es válido 
+        $connObj = new RemoteConnect();
+        $serverName = 'www.google.com';
+        $this->assertTrue($connObj->connectToServer($serverName) !== false);
+        $this->assertTrue($connObj->connectToServer2($serverName) !== false);
+    }
+
+    public function testIncrementSalary() {
+        $connObj = new RemoteConnect();
+        $incrementSalary = $connObj->incrementSalary(1000);
+        $this->assertEquals(20, $incrementSalary);
+    }
+
+  public function testConnectionIsValid2()
+  {
+    // prueba para asegurarse de que el objeto de un fsockopen es válido 
+    $connObj = new RemoteConnect();
+    $serverName = 'www.google.com';
+    $this->assertTrue($connObj->connectToServer2($serverName) !== false);
+  }
+}
