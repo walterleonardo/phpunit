77da2a86be7d9a870b28653f3db66851d29a2ca0
walter leonardo <walterleonardo@gmail.com>
2016-04-08 23:41:09 +0200

    ################## Last version fixed: Date: Autor: Walter Leonardo eMail: walterleonardo@gmail.com Info:
    Bug Info:
    
    
    Solution:
    
    ###################

--- testing/claseAprobarTest.php
+++ testing/claseAprobarTest.php
@@ -35,7 +35,7 @@ class claseAprobarTest extends PHPUnit_Framework_TestCase {
 
     public function testIncrementSalary() {
         $connObj = new RemoteConnect();
-        $incrementSalary = $connObj->incrementSalary(100);
+        $incrementSalary = $connObj->incrementSalary(1000);
         $this->assertEquals(20, $incrementSalary);
     }
     
